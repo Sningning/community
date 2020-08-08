a60acc5ae465a8d2513150edb3a71019c5d8ab8b
Song Ningning <qfsnn1117@gmail.com>
2020-08-08 20:54:25 +0800

    完成显示私信详情功能

--- src/main/java/sningning/community/controller/MessageController.java
+++ src/main/java/sningning/community/controller/MessageController.java
@@ -3,6 +3,7 @@ package sningning.community.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import sningning.community.entity.Message;
@@ -72,5 +73,53 @@ public class MessageController {
         model.addAttribute("letterUnreadCount", letterUnreadCount);
 
         return "/site/letter";
+    }
+
+    /**
+     * 显示私信详情
+     */
+    @RequestMapping(path = "/letter/detail/{conversationId}", method = RequestMethod.GET)
+    public String getLetterDetail(@PathVariable("conversationId") String conversationId, Page page, Model model) {
+
+        User user = hostHolder.getUser();
+
+        // 分页信息
+        page.setLimit(5);
+        page.setPath("/letter/detail/" + conversationId);
+        page.setRows(messageService.findLetterCount(conversationId));
+
+        List<Message> letterList = messageService.findLetters(conversationId, page.getOffset(), page.getLimit());
+        List<Map<String, Object>> letters = new ArrayList<>();
+        if (letterList != null) {
+            for (Message letter : letterList) {
+                Map<String, Object> map = new HashMap<>();
+                map.put("letter", letter);
+                map.put("fromUser", userService.findUserById(letter.getFromId()));
+                letters.add(map);
+            }
+        }
+        model.addAttribute("letters", letters);
+
+        // 私信目标
+        model.addAttribute("target", getLetterTarget(conversationId));
+
+        return "/site/letter-detail";
+    }
+
+    /**
+     * 获得当前私信的对象
+     * @param conversationId
+     * @return 当前私信的对象
+     */
+    private User getLetterTarget(String conversationId) {
+        String[] ids = conversationId.split("_");
+        int id0 = Integer.parseInt(ids[0]);
+        int id1 = Integer.parseInt(ids[1]);
+
+        if (hostHolder.getUser().getId() == id0) {
+            return userService.findUserById(id1);
+        } else {
+            return userService.findUserById(id0);
+        }
     }
 }
