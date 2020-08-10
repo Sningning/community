867910cfc025e9f9ab7e59776fb39cee85ea2fe4
Song Ningning <qfsnn1117@gmail.com>
2020-08-10 23:48:29 +0800

    利用aop统一记录日志

---
+++ src/main/java/sningning/community/aspect/ServiceLogAspect.java
@@ -0,0 +1,50 @@
+package sningning.community.aspect;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * @author: Song Ningning
+ * @date: 2020-08-10 23:33
+ */
+@Component
+@Aspect
+public class ServiceLogAspect {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ServiceLogAspect.class);
+
+    @Pointcut("execution(* sningning.community.service.*.*(..))")
+    public void pointcut() {
+
+    }
+
+    @Before("pointcut()")
+    public void before(JoinPoint joinPoint) {
+        // 用户[192.168.1.1],在[xxx],访问了[sningning.community.service.xxx()]
+
+        // 获取 ip 地址
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        HttpServletRequest request = attributes.getRequest();
+        String ip = request.getRemoteHost();
+
+        // 获取当前时间
+        String now = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
+
+        // 获取具体方法
+        String target = joinPoint.getSignature().getDeclaringTypeName() + "." + joinPoint.getSignature().getName();
+
+        LOGGER.info(String.format("用户[%s], 在[%s], 访问了[%s]", ip, now, target));
+    }
+
+}
