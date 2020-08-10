7da02988c492ee87da014c0b8d915bcbee09e831
Song Ningning <qfsnn1117@gmail.com>
2020-08-10 22:52:56 +0800

    统一处理异常

--- src/main/java/sningning/community/controller/MessageController.java
+++ src/main/java/sningning/community/controller/MessageController.java
@@ -149,6 +149,7 @@ public class MessageController implements CommunityConstant {
     @RequestMapping(path = "/letter/send", method = RequestMethod.POST)
     @ResponseBody
     public String sendLetter(String toName, String content) {
+
         User target = userService.findUserByName(toName);
         if (target == null) {
             return CommunityUtil.getJSONString(-1, "目标用户不存在！");
